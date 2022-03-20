382601fa9e3dec4e95a1aad658bed1b68f861099
Saikat Chakraborty <saikatgpae@gmail.com>
2022-03-20 20:49:20 +0530

    added js1.js inside the static folder

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    name = request.args.get("name")
+    return render_template("index.html", name1=name)
+
+@app.route("greet")
+def greet():
+    return render_template("about.html")
