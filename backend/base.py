d9dffa2aa7b58c1c57565eaba102dde874ed1ea7
Saikat Chakraborty <saikatgpae@gmail.com>
2022-12-10 14:12:42 +0530

    Add backend and frontend setup.

--- backend/base.py
+++ backend/base.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 api = Flask(__name__)
 
-api.route('/profile')
-
+@api.route('/profile')
 def my_profile():
     response_body = {
-        "name": "Saikat Chakraborty",
-        "about": "A full stack developer"
+        "Name": "Saikat Chakraborty",
+        "Qualification" :"BSC CS"
     }
+
     return response_body
